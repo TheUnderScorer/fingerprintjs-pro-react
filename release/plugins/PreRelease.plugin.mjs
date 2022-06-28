6c70038f482c5a19990565d3889e80a0824bec62
Przemysław Żydek <przemyslawzydek@gmail.com>
2022-06-28 11:27:34 +0200

    chore: import package inside method

--- release/plugins/PreRelease.plugin.mjs
+++ release/plugins/PreRelease.plugin.mjs
@@ -1,10 +1,15 @@
 import { Plugin } from "release-it";
-import pkg from "../../package.json" assert { type: "json" };
 import { $ } from "zx";
 
 export default class PreReleasePlugin extends Plugin {
   async afterRelease() {
     const isDryRun = this.config.options["dry-run"];
+
+    const pkg = await import('../../package.json', {
+      assert: {
+        type: 'json'
+      }
+    }).then(module => module.default)
 
     if (!isDryRun) {
       await $`git checkout -b release/${pkg.version}`;
